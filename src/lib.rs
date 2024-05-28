9a0747144ac9afdbf5555c2cbddf81587dd6c281
envsh <yatsen21@outlook.com>
2024-05-28 12:38:28 +0800

    c memory funcs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 
 extern crate libc;
-use libc::{c_void, c_char, malloc, free};
+use libc::{c_void, c_char}; //, malloc, free};
 use std::ffi::CStr;
+use std::ffi::CString;
 
 // the trait `std::default::Default` is implemented for `u8`
 #[derive(Default)]
@@ -16,7 +17,7 @@ pub struct ffiparam {
     len2: usize,
 }
 
-fn dummy_ffifuncproxy_placeholder(v: &ffiparam) {}
+fn dummy_ffifuncproxy_placeholder(_v: &ffiparam) {}
 static mut ffifuncproxy_rs2go : fn(v:&ffiparam) = dummy_ffifuncproxy_placeholder;
 
 /////////////
@@ -31,27 +32,54 @@ pub fn cstrfrom_u8ptr(ptr :*const u8, len: usize) -> &'static str {
     }
 }
 pub fn cstrfrom_u8ptr2(ptr :*const u8, len: usize) -> String {
-    unsafe {
+    // unsafe {
     let xs = cstrfrom_u8ptr(ptr, len);
     
     return xs.to_string()
-    }
+    // }
 }
 pub fn cstrfrom_usizeptr(ptrx :usize, len: usize) -> String {
     let ptr = ptrx as *const u8;
-    unsafe {
+    // unsafe {
     let xs = cstrfrom_u8ptr(ptr, len);
     
     return xs.to_string()
-    }
+    // }
 }
-pub fn refstr2cstru8(v : &str) -> *const u8 {
+// todo
+// pub fn CStringfrom_u8ptr(ptrx :*const u8, len: usize) -> CString {
+    // let vcc  = core::slice::from_raw_parts(ptrx, len+1);
+    // vcc.into()
+    // return ()
+// }
+pub fn refstr2_cstru8(v : &str) -> *const u8 {
     v.as_ptr()
 }
-pub fn refstr2cstrusize(v : &str) -> usize {
+pub fn refstr2_cstrusize(v : &str) -> usize {
     v.as_ptr() as usize
 }
 
+// c memory functions
+pub fn cfree_usize(ptrx : usize) {
+    let ptr = ptrx as *mut c_void;
+    unsafe { libc::free(ptr); }
+}
+pub fn cfree_cvoid(ptr : *mut c_void) {
+    unsafe { libc::free(ptr); }
+}
+pub fn cfree_u8ptr(ptrx : *const u8) {
+    let ptr = ptrx as *mut c_void;
+    unsafe { libc::free(ptr); }
+}
+pub fn cfree_cchar(ptrx : *const c_char) {
+    let ptr = ptrx as *mut c_void;
+    unsafe { libc::free(ptr); }
+}
+// todo
+pub fn cfree<T>(ptrx : T) {
+    // let ptr = ptrx as *mut c_void;
+    // unsafe { libc::free(ptr); }
+}
 
 /////////// template
 pub fn add(left: usize, right: usize) -> usize {
